2069f364a5c2f206c133aefb64e61a37a4f6c528
Alexandre Lebrun <a@lxbrun.com>
2014-09-18 09:19:38 -0700

    qa with stats

--- src/picsou/qa.clj
+++ src/picsou/qa.clj
@@ -4,10 +4,14 @@
 
 (defn go []
   (let [all (j/parse-string (slurp "resources/export.json"))
-        en (get (first (filter #(= "en" (% "body")) all)) "start")]
-    (doseq [[phrase start end] en]
-      (when (<= end (count phrase))
-        (let [s (subs phrase start end)
-              _ (prn s)
-              {:keys [winners] :as res} (p/parse s p/default-context :en$core [{:dim :time :label "T"}])]
-          (prn (map :value winners)))))))
+        en (get (first (filter #(= "en" (% "body")) all)) "start")
+        results (for [[phrase start' end'] en :when (<= end' (count phrase))]
+                  (let [s (subs phrase start' end')
+                        {:keys [winners] :as res} (p/parse s p/default-context :en$core [{:dim :time :label "T"}])
+                        {:keys [start end value] :as first-winner} (first winners)
+                        covers? (and start
+                                     (= 0 start)
+                                     (= (count s) end))]
+                    [s covers? value]))]
+    (prn (count results))
+    (prn (count (filter second results)))))
