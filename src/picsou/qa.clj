41adf35db612bc7d036c946a7e6e38bf096c5099
l5t <laurent@wit.ai>
2014-09-22 09:38:21 -0700

    add module-id for qa

--- src/picsou/qa.clj
+++ src/picsou/qa.clj
@@ -3,19 +3,21 @@
             [clojure.string :as strings]
             [cheshire.core :as j]))
 
-(defn go []
-  (let [all (j/parse-string (slurp "resources/export.json"))
-        en (get (first (filter #(= "fr" (% "body")) all)) "start")
+(defn go
+  ([module-id]
+   (let [all (j/parse-string (slurp "resources/export.json"))
+        en (get (first (filter #(= module-id
+                                   (keyword (str (% "body") "$core"))) all)) "start")
         ;en (take 100 en)
         results (for [[phrase start' end'] en :when (<= end' (count phrase))]
                   (let [s (strings/trim (subs phrase start' end')) ;remove whitespaces
-                        {:keys [winners] :as res} (p/parse s p/default-context :en$core [{:dim :time :label "T"}])
+                        {:keys [winners] :as res} (p/parse s p/default-context module-id [{:dim :time :label "T"}])
                         {:keys [start end value] :as first-winner} (first winners)
                         covers? (and start
                                      (= 0 start)
                                      (= (count s) end))]
                     [s covers? value]))
         ko (remove second results)]
-    (printf "%d/%d passing\n" (- (count results) (count ko)) (count results))
-    (doseq [r ko]
-      (printf "%-40s %s\n" (first r) (-> r last :start)))))
+     (printf "%d/%d passing\n" (- (count results) (count ko)) (count results))
+     (doseq [r ko]
+      (printf "%-40s %s\n" (first r) (-> r last :start))))))
