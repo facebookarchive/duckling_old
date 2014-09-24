01d164611c2ecf649d9f3b49644001b8a041b43f
Alexandre Lebrun <a@lxbrun.com>
2014-09-24 15:06:01 -0700

    feedback in web ui

--- resources/public/out/picsou/try.cljs
+++ resources/public/out/picsou/try.cljs
@@ -37,13 +37,15 @@
         #js {"Content-Type" "application/edn"}))))
 
 (defn get-parse [app owner]
-  (let [phrase (.-value (om/get-node owner "phrase"))
+  (let [phrase (om/get-state owner :text)
+        lang (om/get-state owner :lang)
         now (gdate/DateTime.)
         fnow (str (.toIsoString now) "/" (.getTimezoneOffset now))]
     (.log js/console "Working on: " phrase)
     (.pushState js/history nil nil (str "#" phrase))
+    (put! (om/get-state owner :fb) :reset)
     (edn-xhr {:method "GET"
-              :url (str "parse/" fnow "/" phrase)
+              :url (str "parse/" lang "/" fnow "/" phrase)
               :on-complete #(do (om/update! app [:parse] (first %)))})))
 
 (defn handle-change [e owner {:keys [text] :as state}]
@@ -60,13 +62,59 @@
   (reify
     om/IRender
     (render [this]
-      (token->div cursor))))
+      (dom/div nil
+        (apply dom/div #js {:className "value"}
+          (let [{:keys [start grain end]} (:value cursor)]
+            (if end
+              [(dom/span nil "From ")
+               (dom/span #js {:className "start"} start)
+               (dom/span nil " to ")
+               (dom/span #js {:className "end"} end)
+               (dom/span #js {:className "grain"} (str " " grain))]
+              [(dom/span #js {:className "start"} start)
+               (dom/span #js {:className "grain"} (str " " grain))])))
+          (token->div cursor)))))
+
+(defn- send-feedback [state owner]
+  (.log js/console (:stage state))
+  (case (:stage state)
+    :init (om/set-state! owner :stage :clicked)
+    :clicked (om/set-state! owner :stage :final)))
+
+(defn feedback-view [cursor owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:stage :init
+       :feedback ""})
+    om/IWillMount
+    (will-mount [_]
+      (let [fb-chan (om/get-state owner :fb)]
+        (go (loop []
+          (let [info (<! fb-chan)]
+            (om/set-state! owner :stage :init)
+            (om/set-state! owner :feedback "")
+            (recur))))))
+    om/IRenderState
+    (render-state [_ {:keys [stage feedback] :as state}]
+      (dom/div #js {:className "feedback"}
+        (when (= :clicked stage)
+          (dom/div nil "Thanks, we got your feedback. Feel free to provide additional information:"
+            (dom/textarea #js {:type :text :ref "feedback" :value (:feedback state)
+                            :onChange #(om/set-state! owner :feedback (.. % -target -value))})))
+        (if (#{:init :clicked} stage)
+          (dom/a #js {:onClick #(send-feedback state owner)}
+            (case (:stage state) :init "Flag as incorrect result"
+                                 :clicked "Send"))
+          (dom/div nil "Thanks!"))))))
 
 (defn main-view [app owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:text "next week"})
+      {:text "next week"
+       :lang "en$core"
+       :fb (chan)}) ; channel to communicate with feedback component 
     om/IWillMount
     (will-mount [_]
       (when-let [[_ msg-id] (re-find #"#(.+)$" (.. js/window -location -hash))]
@@ -79,8 +127,15 @@
                           :onChange #(handle-change % owner state)
                           :onKeyDown #(when (= (.-key %) "Enter")
                                         (get-parse app owner))})
+          (dom/select #js {:value (:lang state)
+                           :onChange #(om/set-state! owner :lang (.. % -target -value))}
+                      (dom/option #js {:value "en$core"} "English")
+                      (dom/option #js {:value "cn$core"} "Chinese")
+                      (dom/option #js {:value "es$core"} "Spanish")
+                      (dom/option #js {:value "fr$core"} "French"))
           (dom/button #js {:onClick #(get-parse app owner)} "Try me!"))
-        (dom/div nil (om/build parse-view (:parse app)))))))
+        (om/build parse-view (:parse app))
+        (om/build feedback-view app {:init-state {:fb (:fb state)}})))))
 
 (om/root main-view app-state
   {:target (. js/document (getElementById "main"))})
--- src/picsou/ui/http.clj
+++ src/picsou/ui/http.clj
@@ -7,6 +7,7 @@
   (:require [picsou.core :as pic]
             [clj-time.core :as t]
             [clj-time.format :as f]
+            [clj-time.local :as l]
             [ring.middleware.format-response :refer [wrap-restful-response]]
             [ring.middleware.format-params :refer [wrap-restful-params]]))
 
@@ -16,30 +17,40 @@
 ;     (prn "Chrome Extension request" msg-id)
 ;     (fetch/fetch "a@lxbrun.com" "r0see1888etoile" {:gmail-msg-id msg-id})))
 
+(def date-format (f/formatter "EEEE, MMMM d yyyy"))
+(def time-format (f/formatter " 'at' hh:mm:ss Z (z)"))
+
+(defn- format-date [grain d]
+  (let [ld (l/to-local-date-time d)]
+    (prn ld)
+    (if (#{:year :quarter :month :week :day} grain)
+      (f/unparse date-format ld)
+      (str (f/unparse date-format ld) (f/unparse time-format ld)))))
+
 (defn- trim [token]
   (-> token
       (dissoc :pred)
       (assoc :rule (-> token :rule :name))
       (assoc :route (map trim (:route token)))
-      (update-in-when [:value :start] str)
-      (update-in-when [:value :end] str))) 
+      (update-in-when [:value :start] (partial format-date (-> token :value :grain)))
+      (update-in-when [:value :end] (partial format-date (-> token :value :grain))))) 
 
-(defn parse-controller [{{:keys [phrase reftime reftz]} :params :as params}]
+(defn parse-controller [{{:keys [module phrase reftime reftz]} :params :as params}]
   (prn "Parsing" phrase reftime reftz)
   (let [ref-time (t/from-time-zone
                    (f/parse (f/formatters :basic-date-time-no-ms) (str reftime "Z"))
                    (t/time-zone-for-offset (- (/ (Integer/parseInt reftz) 60))))
         context {:reference-time {:start ref-time :grain :second}}
-        {:keys [winners stash]} (pic/parse phrase context :en$core
+        {:keys [winners stash]} (pic/parse phrase context module
                                            [{:dim :time :label :time}
                                             {:dim :duration :label :period}])]
     (prn ref-time)
-    (prn "Got" (first winners))
+    (prn "Got" (trim (first winners)))
     
     {:body (map trim winners)}))
 
 (defroutes routes
-  (GET "/parse/:reftime/:reftz/:phrase" [] parse-controller)
+  (GET "/parse/:module/:reftime/:reftz/:phrase" [] parse-controller)
   (route/resources "/"))
 
 (defonce stop-http-server (atom nil))
