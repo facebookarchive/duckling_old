83bb15f61bda2015db58417434fe116e5f7e95fe
Alexandre Lebrun <a@lxbrun.com>
2014-09-25 13:49:06 -0700

    midje dep

--- project.clj
+++ project.clj
@@ -4,12 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/wit-ai/picsou"
-  :plugins [[s3-wagon-private "1.1.2"]]
+  :plugins [[s3-wagon-private "1.1.2"]
+            [lein-midje "3.1.3"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/tools.logging "0.2.6"]
                  [clj-time "0.8.0"]
-                 [prismatic/plumbing "0.3.3"]]
+                 [prismatic/plumbing "0.3.3"]
+                 [midje "1.6.3"]]
   :deploy-repositories [["private" {:url "s3p://wit-ai/releases" :username :env :passphrase :env :sign-releases false}]]
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.6"]
                                   [midje "1.6.3"]
