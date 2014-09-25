f24ac5d5867c50d020fa12e3e79b5c7fb63d45bb
Alexandre Lebrun <a@lxbrun.com>
2014-09-25 13:12:48 -0700

    moved http UI to picsou-demo repo

--- project.clj
+++ project.clj
@@ -3,29 +3,11 @@
   :main picsou.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/oliviervaussy/picsou"
-  :cljsbuild {
-    :builds [{
-        :source-paths ["src-cljs"]
-        :compiler {
-          :output-to "resources/public/main.js"  ; default: target/cljsbuild-main.js
-          :output-dir "resources/public/out"
-          :optimizations :none
-          :pretty-print true
-          :source-map true}}]} ;; will add goog/ cljs/ om/ in public
-  :plugins [[s3-wagon-private "1.1.2"]
-            [quickie "0.2.5"]
-            [lein-midje "3.1.3"]
-            [lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :url "https://github.com/wit-ai/picsou"
+  :plugins [[s3-wagon-private "1.1.2"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/clojurescript "0.0-2311"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.7.1"]
-                 [http-kit "2.1.16"]
-                 [compojure "1.1.5"]
-                 [ring-middleware-format "0.4.0"]
                  [clj-time "0.8.0"]
                  [prismatic/plumbing "0.3.3"]]
   :deploy-repositories [["private" {:url "s3p://wit-ai/releases" :username :env :passphrase :env :sign-releases false}]]
