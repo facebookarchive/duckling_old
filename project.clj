f5826a9db25bb229f98b821d0222cae83d8005e4
Alexandre Lebrun <a@lxbrun.com>
2014-09-30 21:57:05 -0700

    project.clj for clojars deploy

--- project.clj
+++ project.clj
@@ -11,10 +11,19 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [clj-time "0.8.0"]
                  [prismatic/plumbing "0.3.3"]]
-  :deploy-repositories [["private" {:url "s3p://wit-ai/releases" :username :env :passphrase :env :sign-releases false}]]
+  :deploy-repositories [["private" {:url "s3p://wit-ai/releases" :username :env :passphrase :env :sign-releases false}]
+                        ["clojars" {:creds :gpg}]]
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.6"]
                                   [midje "1.6.3"]
                                   [cheshire "5.3.1"]]}
              :uberjar {:aot [duckling.core]}}
   :test-selectors {:default (complement :benchmark)
-                   :benchmark :benchmark})
+                   :benchmark :benchmark}
+  :scm {:name "git"
+        :url "https://github.com/wit-ai/duckling"}
+  :signing {:gpg-key "188794F3"}
+  :pom-addition [:developers [:developer
+                               [:name "Wit.ai"]
+                               [:url "https://wit.ai"]
+                               [:email "contact@wit.ai"]
+                               [:timezone "-7"]]])
