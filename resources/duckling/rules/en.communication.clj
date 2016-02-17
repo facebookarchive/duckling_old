d888a7069adb94da427ef5244376c0448c201005
l5t <laurent@wit.ai>
2016-02-17 15:48:45 -0800

    add specific rule for dictated phone numbers in EN

--- resources/duckling/rules/en.communication.clj
+++ resources/duckling/rules/en.communication.clj
@@ -3,6 +3,13 @@
  #"(?i)(\+?[0-9\(][0-9\- \(\)\.]{6,16}( ?e?xt?\.? ?\d+)?)"
  {:dim :phone-number
   :value (-> %1 :groups first)}
+
+  "phone number given by each number" ;;eight nine nine one two six seven four three nine
+  [(integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9) 
+  	(integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9)] 
+  {:dim :phone-number
+  :value (clojure.string/join "" [(:value %1) (:value %2) (:value %3) (:value %4) ;; To be improved
+  			(:value %5) (:value %6) (:value %7) (:value %8) (:value %9) (:value %10)])}
  
  "url"
  #"((https?://)?(\w+\.)+\w{2,4}(:\d+)?[^ ]*)"
