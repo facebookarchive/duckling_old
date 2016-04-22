98b280be4b54271cd3ebf75c3653b490816a9de2
Jim O'Regan <joregan@gmail.com>
2016-04-22 20:01:04 +0100

    more time

--- resources/duckling/rules/ga.time.clj
+++ resources/duckling/rules/ga.time.clj
@@ -62,4 +62,76 @@
   "named-day"
   #"(?i)domhnai?[cg]h|do\.?"
   (day-of-week 7)
+
+  "named-month"
+  #"(?i)(mh?í )?(an )?t?ean[áa]ir|ean\.?"
+  (month 1)
+
+  "named-month"
+  #"(?i)(mh?í )?(na )?feabhra|fea\.?"
+  (month 2)
+
+  "named-month"
+  #"(?i)(mh?í )?(an )?mh?[áa]rta|m[áa]r\.?"
+  (month 3)
+
+  "named-month"
+  #"(?i)(mh?í )?(an )?t?aibre[áa]i?n|abr\.?"
+  (month 4)
+
+  "named-month"
+  #"(?i)(mh?í )?(na )?bh?ealtaine|bea\.?"
+  (month 5)
+
+  "named-month"
+  #"(?i)(mh?í )?(an )?mh?eith(ea|i)mh|mei\.?"
+  (month 6)
+
+  "named-month"
+  #"(?i)(mh?í )?i[úu]il|iúi\.?"
+  (month 7)
+
+  "named-month"
+  #"(?i)(mh?í )?(na )?l[úu]nasa|l[úu]n\.?"
+  (month 8)
+
+  "named-month"
+  #"(?i)(mh?í )?mh?e[áa]n f[óo]mhair|mef?\.?"
+  (month 9)
+
+  "named-month"
+  #"(?i)(mh?í )?dh?eireadh f[óo]mhair|def?\.?"
+  (month 10)
+
+  "named-month"
+  #"(?i)(mh?í )?(na )?samh(ain|na)|sam\.?"
+  (month 11)
+
+  "named-month"
+  #"(?i)(mh?í )?(na )?nollai?g|nol\.?"
+  (month 12)
+
+  ; Holiday TODO: check online holidays
+  ; or define dynamic rule (last thursday of october..)
+
+  "An Nollaig" ; "Mí na Nollag" is literally "month of Christmas", so collision with month
+  #"(?i)(l[áa] |an )?(nollai?g)"
+  (month-day 12 25)
+
+  "Nollaig na mBan"
+  #"(?i)(l[áa] |an )?nollaig (bheag|na mban)"
+  (month-day 1 6)
+
+  "Lá Fhéile Vailintín"
+  #"(?i)(l[áa] )?fh[eé]ile|'?le vailint[íi]n"
+  (month-day 2 14)
+
+  "Lá na nAithreacha"; third Sunday of June
+  #"(?i)l[áa] na naithreacha"
+  (intersect (day-of-week 7) (month 6) (cycle-nth-after :week 2 (month-day 6 1)))
+
+;  "Mother's Day"; fourth Sunday of Lent. Need Lent.
+;  #"(?i)mother'?s? day"
+;  (intersect (day-of-week 7) (month 5) (cycle-nth-after :week 1 (month-day 5 1)))
+
 )
