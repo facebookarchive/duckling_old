9749100f4bd519ca7e91d3eb14509fd0674e05c6
Jim O'Regan <joregan@gmail.com>
2016-04-25 22:11:52 +0100

    remove bits now missing in en

--- resources/languages/ga/corpus/measure.clj
+++ resources/languages/ga/corpus/measure.clj
@@ -2,19 +2,6 @@
   ; Context map
   {}
 
-  "míle"
-  "mhíle"
-  (distance :length 1609.34)
-
-  "km"
-  "ciliméadar"
-  "ciliméadair"
-  (distance :length 1000)
-
-  "méadar"
-  "méadair"
-  (distance :length 1)
-
   "4 mhíle"
   "4 míle"
   (distance :length 6437.36)
