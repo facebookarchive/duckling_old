a182649893263ce2f19554baed08771d3c0bbc82
Jim O'Regan <joregan@gmail.com>
2016-04-26 10:51:38 +0100

    foot/inch

--- resources/languages/ga/rules/measure.clj
+++ resources/languages/ga/rules/measure.clj
@@ -1,4 +1,11 @@
 (
+  ; latent distance
+  "number as distance"
+  (dim :number)
+  {:dim :distance
+   :latent true
+   :value (:value %1)}
+
   "<dist> meters"
   [(dim :distance) #"(?i)mh?[eé]adai?r"]
   (-> %1
@@ -22,11 +29,26 @@
                            :unit "metre"}}))
 
   "<latent dist> km"
-  [(dim :distance) #"(?i)km|g?ch?ilim[eé]dai?r"]
+  [(dim :distance) #"(?i)k\.?m\.?|g?ch?ilim[eé]dai?r"]
   (-> %1
       (dissoc  :latent)
       (merge {:unit "kilometre"
               :normalized {:value (* 1000 (-> %1 :value))
                            :unit "metre"}}))
 
+  "<latent dist> troigh"
+  [(dim :distance) #"(?i)('|troi[tg]he?|tr\.?"]
+  (-> %1
+      (dissoc  :latent)
+      (merge {:unit "foot"
+              :normalized {:value (* 0.3048 (-> %1 :value))
+                           :unit "metre"}}))
+
+  "<latent dist> orlach"
+  [(dim :distance) #"(?i)(''|([nth]-?)?orl(ach|aigh|a[íi]|\.)"]
+  (-> %1
+      (dissoc  :latent)
+      (merge {:unit "inch"
+              :normalized {:value (* 0.0254 (-> %1 :value))
+                           :unit "metre"}}))
 )
