a6d5e0d53791b63c4bf65cc71734b5542694e98e
Jim O'Regan <joregan@gmail.com>
2016-05-12 11:52:04 +0100

    more

--- resources/languages/ga/rules/measure.clj
+++ resources/languages/ga/rules/measure.clj
@@ -51,4 +51,66 @@
       (merge {:unit "inch"
               :normalized {:value (* 0.0254 (-> %1 :value))
                            :unit "metre"}}))
+
+  "<dist> m (ambiguous miles or meters)"
+  [(dim :distance) #"(?i)m"]
+  (-> %1
+      (dissoc :latent)
+      (merge {:unit "m"}))
+
+  ;; volume
+
+  ; latent volume
+  "number as volume"
+  (dim :number)
+  {:dim :volume
+   :latent true
+   :value (:value %1)}
+
+  "<latent vol> ml"
+  [(dim :volume) #"(?i)m(l\.?|h?illil[íi]t(ea|i)r)"]
+  (-> %1
+      (dissoc  :latent)
+      (merge {:unit "millilitre"
+              :normalized {:value (* 0.001 (-> %1 :value))
+                           :unit "litre"}}))
+
+  "<latent vol> kl"
+  [(dim :volume) #"(?i)(kl\.?|g?ch?illil[íi]t(ea|i)r)"]
+  (-> %1
+      (dissoc  :latent)
+      (merge {:unit "millilitre"
+              :normalized {:value (* 1000 (-> %1 :value))
+                           :unit "litre"}}))
+
+  "<vol> heictilítir"
+  [(dim :volume) #"(?i)heictil[íi]t(ea|i)r"]
+  (-> %1
+      (dissoc  :latent)
+      (merge {:unit "hectolitre"
+              :normalized {:value (* 100 (-> %1 :value))
+                           :unit "litre"}}))
+
+  "<vol> lítear"
+  [(dim :volume) #"(?i)(l[íi]t(ea|i)r|l\.?)"]
+  (-> %1
+      (dissoc  :latent)
+      (merge {:unit "litre"}))
+
+  "half leathlítear"
+  [#"(?i)leathl[íi]t(ea|i)r"]
+  (-> %1
+      (dissoc  :latent)
+      (merge {:unit "litre"
+              :value 0.5}))
+
+  "<latent vol> galún"
+  [(dim :volume) #"(?i)n?gh?al[úu]i?n"]
+  (-> %1
+      (dissoc  :latent)
+      (merge {:unit "gallon"
+              :normalized {:value (* 3.785 (-> %1 :value))
+                           :unit "litre"}}))
+
+
 )
