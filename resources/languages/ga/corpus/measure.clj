3a98cbe9f9b107b9517f7bf41745aee5bd632b22
Jim O'Regan <joregan@gmail.com>
2016-05-12 11:56:26 +0100

    more

--- resources/languages/ga/corpus/measure.clj
+++ resources/languages/ga/corpus/measure.clj
@@ -16,4 +16,26 @@
   "8 mhíle"
   "8 míle"
   (distance 8 "mile" {:value 12872 :unit "metre"})
+
+  "9m"
+  (distance 9 "m")
+
+  "2cm"
+  "2 cheintiméadar"
+  (distance 2 "centimetre" {:value 0.02 :unit "metre"})
+
+  ; Volume
+
+  "250 millilítir"
+  "250 millilitir"
+  "250ml"
+  "250 ml"
+  (volume  250 "millilitre" {:value 0.25 :unit "litre"})
+
+  "2 lítir"
+  (volume 2 "litre")
+
+  "5 galúin"
+  (volume 5 "gallon" {:value 11.355 :unit "litre"})
+
 )
