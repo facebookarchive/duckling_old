76192e05c8f540f81802ca54b604aca1c0adbc6a
Jim O'Regan <joregan@gmail.com>
2016-06-21 15:59:24 +0100

    copy from English

---
+++ resources/languages/ga/corpus/finance.clj
@@ -0,0 +1,75 @@
+(
+
+{}
+
+"$10"
+"10$"
+"ten dollars"
+(money 10 "$")
+
+;"under $15"
+;"no more than 15$"
+;"no greater than 15$"
+;"less than fifteen dollars"
+;(money 15 "$" "<")
+
+"ten cents"
+(money 10 "cent")
+
+"$10,000"
+"10K$"
+"$10k"
+(money 10000 "$")
+
+"USD1.23"
+(money 1.23 "USD")
+
+"2 dollar and 23 cents"
+"two dollar 23 cents"
+"2 dollar 23"
+"two dollar and 23"
+(money 2.23 "$")
+
+"20€"
+"20 euros"
+"20 Euro"
+"20 Euros"
+"EUR 20"
+(money 20 "EUR")
+
+"EUR29.99"
+(money 29.99 "EUR")
+
+;Indian Currency
+"Rs. 20"
+"Rs 20"
+"20 Rupees"
+"20Rs"
+"Rs20"
+(money 20 "INR")
+
+"20 Rupees 43"
+"twenty rupees 43"
+(money 20.43 "INR")
+
+"INR33"
+(money 33 "INR")
+
+"3 bucks"
+(money 3) ; unknown unit
+
+"£9"
+"nine pounds"
+(money 9 "£")
+
+"GBP3.01"
+"GBP 3.01"
+(money 3.01 "GBP")
+
+)
+
+;around $200
+;between $200 and $300
+;around 200-500 dollars
+;up to 1000
+;~50-100
