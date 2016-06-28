dd4c5ae27e71d79a0d1a51de7e9f4457cfe27e6c
l5t <laurent@wit.ai>
2016-06-28 07:54:26 -0700

    bumped 0.4.6 to include fix on 12am/12pm for EN

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wit/duckling "0.4.5"
+(defproject wit/duckling "0.4.6"
   :description "Date & Number parser"
   :license {:url "https://github.com/wit-ai/duckling"
             :comments "see LICENSE"}
