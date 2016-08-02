fcde049e1533c340751ad53c15a2137e4f60d8cf
icalF <laser.survivor@gmail.com>
2016-08-02 19:41:13 +0700

    Extend new language : Indonesian but need more fix

---
+++ resources/languages/id/corpus/finance.clj
@@ -0,0 +1,64 @@
+(
+
+{}
+
+"$10"
+"10$"
+"sepuluh dolar"
+(money 10 "$")
+
+;"under $15"
+;"no more than 15$"
+;"no greater than 15$"
+;"less than fifteen dollars"
+;(money 15 "$" "<")
+
+"$10.000"
+"10K$"
+"$10k"
+(money 10000 "$")
+
+"USD1,23"
+(money 1.23 "USD")
+
+"2 dolar"
+"dua dolar"
+(money 2 "$")
+
+"20€"
+"20 euros"
+"20 Euro"
+"20 Euros"
+"EUR 20"
+(money 20 "EUR")
+
+"EUR29,99"
+(money 29.99 "EUR")
+
+;Indian Currency
+"Rp. 20,00"
+"Rp 20"
+"20 Rupiah"
+"20Rp"
+"Rp20"
+(money 20 "IDR")
+
+"IDR33"
+"IDR 35.000"
+(money 33 "IDR")
+
+"£9"
+"sembilan pound"
+(money 9 "£")
+
+"GBP3,01"
+"GBP 3,01"
+(money 3.01 "GBP")
+
+)
+
+;around $200
+;between $200 and $300
+;around 200-500 dollars
+;up to 1000
+;~50-100
