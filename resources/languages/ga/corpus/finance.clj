489ae42d00805ba46722669f37ac61b5748a54c8
Jim Regan <joregan@gmail.com>
2016-09-28 17:15:19 +0100

    Irish (#94)
    * start of Irish (ga)
    
    * metrics
    
    * more time
    
    * move to new structure
    
    * lf
    
    * finance
    
    * more finance
    
    * basic start to numbers, but Irish has a quite complicated system
    
    * files renamed
    
    * update
    
    * update
    
    * stupid copy/paste errors
    
    * more times stuff
    
    * missing brackets
    
    * rearrange cardinals a little better to try to avoid collisions in the regex
    
    * alternate
    
    * fix name collision, still broken
    
    * remove bits now missing in en
    
    * more missing brackets, add numeric integers
    
    * move, see if that has an effect
    
    * still not working
    
    * add start of numbers
    
    * move ordinals higher, because many of them are the same as cardinals with -ú. Still fails to parse, but /play shows that the match is happening
    
    * foot/inch
    
    * forgot initial mutation
    
    * also forgot to close the bracket
    
    * optional .
    
    * a couple more named days
    
    * fix
    
    * start of cycles
    
    * fix regexes
    
    * fix more regexes
    
    * fix more regexes
    
    * start of durations
    
    * fix a regex, add some stuff
    
    * still broken. Fails to match, not sure how to get at that
    
    * fixed: match on :text, not :body
    
    * accept dhá
    
    * ag an am seo is more likely to mean anois; add arú amárach
    
    * more time
    
    * fix comment
    
    * formatted dates (dd/mm/yyyy etc.)
    
    * add .lein-failures
    
    * spelling
    
    * add more
    
    * more
    
    * more
    
    * leathghalún
    
    * copy from English
    
    * fix comment
    
    * adapt a little
    
    * arú amárach - dunno if I remembered to add the corresponding rule
    
    * missing "?"
    
    * add one
    
    * convert some more english
    
    * convert some more english
    
    * convert some more english
    
    * convert some more english
    
    * more number rules, mostly copied from English, because we have the same conventions
    
    * copy suffixes rule; may need expansion, but the English abbreviations are very much in use
    
    * another test, to see if it's just dollars
    
    * €10,000 not parsing; dunno why, disabling for now
    
    * fix order in regex
    
    * copy another rule for numbers from en
    
    * re-enable $10,000 test
    
    * fix de luain seo chaite

---
+++ resources/languages/ga/corpus/finance.clj
@@ -0,0 +1,80 @@
+(
+
+{}
+
+"$10"
+"10$"
+"deich dollair"
+(money 10 "$")
+
+;"under $15"
+;"no more than 15$"
+;"no greater than 15$"
+;"less than fifteen dollars"
+;(money 15 "$" "<")
+
+"deich ceinteanna"
+(money 10 "cent")
+
+"$10,000"
+"10K$"
+"$10k"
+(money 10000 "$")
+
+"€10,000"
+"10K€"
+"€10k"
+(money 10000 "EUR")
+
+"USD1.23"
+(money 1.23 "USD")
+
+"2 dhollair agus 23 ceinteanna"
+"dhá dhollair 23 ceinteanna"
+"2 dhollair 23"
+"dhá dhollair agus 23"
+(money 2.23 "$")
+
+"20€"
+"20 euros"
+"20 Euro"
+"20 Euros"
+"EUR 20"
+(money 20 "EUR")
+
+"EUR29.99"
+(money 29.99 "EUR")
+
+;Indian Currency
+"Rs. 20"
+"Rs 20"
+"20 Rúpaí"
+"20Rs"
+"Rs20"
+(money 20 "INR")
+
+"20 Rupees 43"
+"fiche rúpaí 43"
+(money 20.43 "INR")
+
+"INR33"
+(money 33 "INR")
+
+;"3 bucks"
+;(money 3) ; unknown unit
+
+"£9"
+"naoi bpunt"
+(money 9 "£")
+
+"GBP3.01"
+"GBP 3.01"
+(money 3.01 "GBP")
+
+)
+
+;around $200
+;between $200 and $300
+;around 200-500 dollars
+;up to 1000
+;~50-100
