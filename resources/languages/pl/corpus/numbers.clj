d2956d6bc9c3e18a12ab1cee9c6337cb34b3cf0f
Andrzej Raczkowski <araczkowski@gmail.com>
2016-11-17 06:25:39 +0100

    pl numbers (#170)
    thanks for your contribution

---
+++ resources/languages/pl/corpus/numbers.clj
@@ -0,0 +1,103 @@
+(
+  ; Context map
+  {}
+
+  "0"
+  "nic"
+  "zero"
+  (number 0)
+
+  "1"
+  "jeden"
+  "pojedynczy"
+  (number 1)
+
+  "2"
+  "dwa"
+  "para"
+  (number 2)
+
+  "33"
+  "trzydzieści trzy"
+  "0033"
+  (number 33)
+
+  "14"
+  "czternaście"
+  (number 14)
+
+  "16"
+  "szesnaście"
+  (number 16)
+
+  "17"
+  "siedemnaście"
+  (number 17)
+
+  "18"
+  "osiemnaście"
+  (number 18)
+
+  "1.1"
+  "1.10"
+  "01.10"
+  (number 1.1)
+
+  "0.77"
+  ".77"
+  (number 0.77)
+
+  "100,000"
+  "100000"
+  "100K"
+  "100k"
+  (number 100000)
+
+  "3M"
+  "3000K"
+  "3000000"
+  "3,000,000"
+  (number 3000000)
+
+  "1,200,000"
+  "1200000"
+  "1.2M"
+  "1200K"
+  (number 1200000)
+
+  "- 1,200,000"
+  "-1200000"
+  "minus 1,200,000"
+  "-1.2M"
+  "-1200K"
+  "-.0012G"
+  (number -1200000)
+
+  "5 tysięcy"
+  "pięć tysięcy"
+  (number 5000)
+
+  "sto dwadzieścia dwa"
+  (number 122)
+
+  "dwieście tysięcy"
+  (number 200000)
+
+  "dwadzieścia jeden tysięcy i jedenaście"
+  "dwadzieścia jeden tysięcy jedenaście"
+  (number 21011)
+
+  "siedemset dwadzieścia jeden tysięcy dwanaście"
+  "siedemset dwadzieścia jeden tysięcy i dwanaście"
+  (number 721012)
+
+  "sześćdziesiąt pięć milionów"
+  (number 65000000)
+
+  "trzydzieści jeden milionów dwieście pięćdziesiąt sześć tysięcy siedemset dwadzieścia jeden"
+  (number 31256721)
+
+  "4ty"
+  "czwarty"
+  (ordinal 4)
+)
