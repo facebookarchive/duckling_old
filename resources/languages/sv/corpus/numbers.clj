1b52a488617b602779acf13d416d240d2ea9ac0e
Magnus Burton <magnus.burton@hotmail.com>
2016-11-17 06:32:46 +0100

    Add Swedish language (#174)
    * Added Swedish rules and corpus
    
    * Fixed spelling

---
+++ resources/languages/sv/corpus/numbers.clj
@@ -0,0 +1,87 @@
+(
+  ; Context map
+  {}
+
+  "0"
+  "noll"
+  (number 0)
+
+  "1"
+  "en"
+  "ett"
+  (number 1)
+
+  "2"
+  "två"
+  "ett par"
+  (number 2)
+
+  "7"
+  "sju"
+  (number 7)
+
+  "14"
+  "fjorton"
+  (number 14)
+
+  "16"
+  "sexton"
+  (number 16)
+
+  "17"
+  "sjutton"
+  (number 17)
+
+  "18"
+  "arton"
+  (number 18)
+
+  "20"
+  "tjugo"
+  (number 20)
+
+  "1,1"
+  "1,10"
+  "01,10"
+  (number 1.1)
+
+  "0,77"
+  ",77"
+  (number 0.77)
+
+  "100.000"
+  "100000"
+  "100K"
+  "100k"
+  (number 100000)
+
+  "3M"
+  "3000K"
+  "3000000"
+  "3.000.000"
+  (number 3000000)
+
+  "1.200.000"
+  "1200000"
+  "1,2M"
+  "1200K"
+  ",0012G"
+  (number 1200000)
+
+  "- 1.200.000"
+  "-1200000"
+  "minus 1.200.000"
+  "minus 1200000"
+  "-1,2M"
+  "-1200K"
+  "-,0012G"
+  (number -1200000)
+
+  "5 tusen"
+  "fem tusen"
+  (number 5000)
+
+  "4."
+  "fjärde"
+  (ordinal 4)
+)
---
+++ resources/languages/sv/rules/cycles.clj
@@ -0,0 +1,121 @@
+; Cycles are like a heart beat, the next starts just when the previous ends.
+; Unlike durations, they have an absolute position in the time, it's just that this position is periodic.
+; Examples of phrases involving cycles:
+; - this week
+; - today (= this day)
+; - last month
+; - last 2 calendar months (last 2 months is interpreted as a duration)
+;
+; As soon as you put a quantity (2 months), the cycle becomes a duration.
+
+
+(
+  "second (cycle)"
+  #"(?i)sekund(er)?"
+  {:dim :cycle
+   :grain :second}
+
+  "minute (cycle)"
+  #"(?i)minut(er)?"
+  {:dim :cycle
+   :grain :minute}
+
+  "hour (cycle)"
+  #"(?i)timm(ar|e)?"
+  {:dim :cycle
+   :grain :hour}
+
+  "day (cycle)"
+  #"(?i)dag(ar)?"
+  {:dim :cycle
+   :grain :day}
+
+  "week (cycle)"
+  #"(?i)veck(or|a)?"
+  {:dim :cycle
+   :grain :week}
+
+  "month (cycle)"
+  #"(?i)månad(er)?"
+  {:dim :cycle
+   :grain :month}
+
+  "quarter (cycle)"
+  #"(?i)kvartal(et)?"
+  {:dim :cycle
+   :grain :quarter}
+
+  "year (cycle)"
+  #"(?i)år"
+  {:dim :cycle
+   :grain :year}
+
+  "this <cycle>"
+  [#"(?i)denna|detta|i|nuvarande" (dim :cycle)]
+  (cycle-nth (:grain %2) 0)
+
+  "last <cycle>"
+  [#"(?i)sista|senaste|förra" (dim :cycle)]
+  (cycle-nth (:grain %2) -1)
+
+  "next <cycle>"
+  [#"(?i)nästa|kommande" (dim :cycle)]
+  (cycle-nth (:grain %2) 1)
+
+  "the <cycle> after <time>"
+  [(dim :cycle) #"(en|et)? (?i)efter" (dim :time)]
+  (cycle-nth-after (:grain %1) 1 %3)
+
+  "<cycle> after <time>"
+  [(dim :cycle) #"(?i)efter" (dim :time)]
+  (cycle-nth-after (:grain %1) 1 %3)
+
+  "the <cycle> before <time>"
+  [(dim :cycle) #"(en|et)? (?i)före" (dim :time)]
+  (cycle-nth-after (:grain %1) -1 %3)
+
+  "<cycle> before <time>"
+  [(dim :cycle) #"(?i)före" (dim :time)]
+  (cycle-nth-after (:grain %1) -1 %3)
+
+  "last n <cycle>"
+  [#"(?i)sista|senaste|förra" (integer 1 9999) (dim :cycle)]
+  (cycle-n-not-immediate (:grain %3) (- (:value %2)))
+
+  "next n <cycle>"
+  [#"(?i)nästa" (integer 1 9999) (dim :cycle)]
+  (cycle-n-not-immediate (:grain %3) (:value %2))
+
+  "<ordinal> <cycle> of <time>"
+  [(dim :ordinal) (dim :cycle) #"(?i)av|i|från" (dim :time)]
+  (cycle-nth-after-not-immediate (:grain %2) (dec (:value %1)) %4)
+
+  "the <ordinal> <cycle> of <time>"
+  [#"(?i)den" (dim :ordinal) (dim :cycle) #"(?i)av|i|från" (dim :time)]
+  (cycle-nth-after-not-immediate (:grain %3) (dec (:value %2)) %5)
+
+  "the <cycle> of <time>"
+  [(dim :cycle) #"(en|et)? (?i)av" (dim :time)]
+  (cycle-nth-after-not-immediate (:grain %1) 0 %3)
+
+  ; the 2 following rules may need a different helper
+
+  "<ordinal> <cycle> after <time>"
+  [(dim :ordinal) (dim :cycle) #"(?i)efter" (dim :time)]
+  (cycle-nth-after-not-immediate (:grain %2) (dec (:value %1)) %4)
+
+  "the <ordinal> <cycle> after <time>"
+  [#"(?i)den" (dim :ordinal) (dim :cycle) #"(?i)efter" (dim :time)]
+  (cycle-nth-after-not-immediate (:grain %3) (dec (:value %2)) %5)
+
+
+  ; quarters are a little bit different, you can say "3rd quarter" alone
+
+  "<ordinal> quarter"
+  [(dim :ordinal) (dim :cycle #(= :quarter (:grain %)))]
+  (cycle-nth-after :quarter (dec (:value %1)) (cycle-nth :year 0))
+
+  "<ordinal> quarter <year>"
+  [(dim :ordinal) (dim :cycle #(= :quarter (:grain %))) (dim :time)]
+  (cycle-nth-after :quarter (dec (:value %1)) %3)
+)
