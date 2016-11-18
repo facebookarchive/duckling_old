b5a367a81d16d82eb91a88876d45e060abf7a6af
l5t <laurent@wit.ai>
2016-11-17 19:46:56 -0800

    bumping version to 0.4.20

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wit/duckling "0.4.19"
+(defproject wit/duckling "0.4.20"
   :description "Date & Number parser"
   :license {:url "https://github.com/wit-ai/duckling"
             :comments "see LICENSE"}
