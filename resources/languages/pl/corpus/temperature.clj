2bdf2c030a693895e38e5c22ad1b3e02526401b6
Kuba Wolanin <hi@kubawolanin.com>
2016-11-22 19:14:46 +0100

    [PL] Temperature

---
+++ resources/languages/pl/corpus/temperature.clj
@@ -0,0 +1,26 @@
+(
+	; Context map
+	{}
+
+	; Temperature
+
+	"37°C"
+	"37 ° celsjusza"
+	"37 stopni Celsjusza"
+	"trzydzieści siedem celsjuszy"
+	"37 st. celsjusza"
+	"trzydzieści siedem stopni celsjusza"
+	(temperature 37 "celsius")
+
+	"70°F"
+	"70 ° Fahrenheita"
+	"70 stopni F"
+	"siedemdziesiąt Fahrenheitów"
+	(temperature 70 "fahrenheit")
+	             
+	"45°"
+	"45 stopni"
+	"45 st."
+	(temperature 45 "degree")
+
+)
