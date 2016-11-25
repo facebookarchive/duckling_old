799833b0f3843a804c2e0637963d7334ad9344cb
uyavuz <utku@Fikris-MacBook-Pro.local>
2016-11-25 22:38:51 +0300

    Turkish Support - Numbers
    Added support for numbers in Turkish language. Rules are created based
    on the English version.

---
+++ resources/languages/tr/corpus/numbers.clj
@@ -0,0 +1,164 @@
+(
+  ; Context map
+  {}
+
+  "0"
+  "sıfır"
+  "hiç"
+  (number 0)
+
+  "1"
+  "bir"
+  "tek"
+  "yek"
+  (number 1)
+
+  "2"
+  "iki"
+  "çift"
+  (number 2)
+
+  "33"
+  "otuzüç"
+  "otuz üç"
+  "0033"
+  (number 33)
+  
+  "14"
+  "ondört"
+  "on dört"
+  (number 14)
+  
+  "16"
+  "onaltı"
+  "on altı"
+  (number 16)
+
+  "17"
+  "onyedi"
+  "on yedi"
+  (number 17)
+
+  "18"
+  "onsekiz"
+  "on sekiz"
+  (number 18)
+
+  "1.1"
+  "1.10"
+  "01.10"
+  "bir virgül bir"
+  "bir nokta bir"
+  (number 1.1)
+
+  "0.77"
+  ".77"
+  (number 0.77)
+  
+  "100,000"
+  "100000"
+  "100K"
+  "100k"
+  "100b"
+  "100K"
+  (number 100000)
+  
+  "3M"
+  "3000K"
+  "3000000"
+  "3,000,000"
+  (number 3000000)
+  
+  "1,200,000"
+  "1200000"
+  "1.2M"
+  "1200K"
+  ".0012G"
+  "1200B"
+  (number 1200000)
+
+  "- 1,200,000"
+  "-1200000"
+  "eksi 1,200,000"
+  "negatif 1200000"
+  "-1.2M"
+  "-1200K"
+  "-.0012G"
+  "-1200B"
+  (number -1200000)
+
+  "5 bin"
+  "beş bin"
+  (number 5000)
+  
+  "5 deste"
+  "beş deste"
+  (number 50)
+
+  "iki yüz bin"
+  "ikiyüzbin"
+  (number 200000)
+
+  "yirmi bir bin on bir"
+  "yirmibir bin onbir"
+  (number 21011)
+
+  "yedi yüz yirmibir bin on iki"
+  "yedi yüz yirmi bir bin on iki"
+  "yediyüz yirmibir bin oniki"
+  (number 721012)
+  
+  "üçyüzbin üçyüz kırkbir"
+  "üç yüz bin üç yüz kırk bir"
+  (number 300341)
+  
+  "kırkbin üçyüz kırksekiz"
+  "kırk bin üç yüz kırk sekiz"
+  (number 40348)
+
+  "otuz bir milyon iki yüz elli altı bin yedi yüz yirmi bir"
+  (number 31256721)
+
+  "4üncü"
+  "dördüncü"
+  "4."
+  "4'üncü"
+  (ordinal 4)
+  
+  "107"
+  "yüz yedi"
+  (number 107)
+  
+  "beş buçuk"
+  "beşbuçuk"
+  "5 buçuk"
+  "5.5"
+  (number 5.5)
+  
+  "3.5 milyon"
+  "3500000"
+  "üç buçuk milyon"
+  "üçbuçuk milyon"
+  "3.5M"
+  (number 3500000)
+  
+  "yarım"
+  "0.5"
+  (number 0.5)
+  
+  "2.5 bin"
+  "2500"
+  "iki buçuk bin"
+  "ikibuçuk bin"
+  (number 2500)
+  
+  "2.2 milyon"
+  "iki nokta iki milyon"
+  (number 2200000)
+  
+  "yetmişikibuçuk"
+  "yetmişiki buçuk"
+  "72.5"
+  (number 72.5)
+)
+
