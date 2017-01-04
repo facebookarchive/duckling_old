208ed082954abfc58e7bde02a8ab44a5707cbbd0
Robin van der Vleuten <robin@webstronauts.co>
2017-01-04 15:35:36 +0100

    Added rules for Dutch numbers.

---
+++ resources/languages/nl/corpus/numbers.clj
@@ -0,0 +1,75 @@
+(
+  ; Context map
+  {}
+
+  "0"
+  "nul"
+  "geen"
+  "niks"
+  (number 0)
+
+  "1"
+  "een"
+  "één"
+  (number 1)
+
+  "2"
+  "twee"
+  (number 2)
+
+  "33"
+  "3 en 30"
+  "drieendertig"
+  "drieëndertig"
+  "drie en dertig"
+  "0033"
+  (number 33)
+
+  "14"
+  "veertien"
+  (number 14)
+
+  "16"
+  "zestien"
+  (number 16)
+
+  "17"
+  "zeventien"
+  (number 17)
+
+  "18"
+  "achtien"
+  (number 18)
+
+  "1,1"
+  "1,10"
+  "01,10"
+  (number 1.1)
+
+  "0,77"
+  ",77"
+  (number 0.77)
+
+  "5 duizend"
+  "vijf duizend"
+  "vijfduizend"
+  (number 5000)
+
+  "honderdtweeentwintig"
+  "honderdtweeëntwintig"
+  "honderd tweeëntwintig"
+  "honderd tweeentwintig"
+  "honderd twee en twintig"
+  (number 122)
+
+  "twintigduizend"
+  "twintig duizend"
+  (number 200000)
+
+  "eenentwintigduizend en elf"
+  (number 21011)
+
+  "4de"
+  "vierde"
+  (ordinal 4)
+)
