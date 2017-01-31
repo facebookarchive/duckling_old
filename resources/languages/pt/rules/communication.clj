a3365802a7dac62ce89b1f0ff19bcfbb1a7c7db6
gnardari <guilhermenardari@gmail.com>
2017-01-31 14:18:10 -0200

    [PT] updating communication rules based on the EN file

--- resources/languages/pt/rules/communication.clj
+++ resources/languages/pt/rules/communication.clj
@@ -1,18 +1,18 @@
 (
  "phone number"
- #"(?i)(\+?[0-9\(][0-9\- \(\)\.]{6,16}( ?e?xt?\.? ?\d+)?)"
+ #"(?i)(\+?[0-9\(][0-9\- \(\)\.]{6,16}( ?(ramal) ?\d+)?)"
  {:dim :phone-number
   :value (-> %1 :groups first)}
 
   "phone number given by each number" ;;eight nine nine one two six seven four three nine
-  [(integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9) 
-  	(integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9)] 
+  [(integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9)
+  	(integer 0 9) (integer 0 9) (integer 0 9) (integer 0 9)]
   {:dim :phone-number
   :value (clojure.string/join "" [(:value %1) (:value %2) (:value %3) (:value %4) ;; To be improved
   			(:value %5) (:value %6) (:value %7) (:value %8) (:value %9) (:value %10)])}
- 
+
  "url"
- #"((https?://)?(\w+\.)+\w{2,4}(:\d+)?[^ ]*)"
+ #"(\b(?<![@.,%&#-])(\w{2,10}://)?((?:\w|\&\#\d{3,5};)[.-]?)+\.([a-z]{2,15})\b(?![@])(/)?(?:([\w\d\?\-=#:%@&.;])+(?:/(?:([\w\d\?\-=#:%@&;.])+))*)?(?<![.,?!-]))"
  {:dim :url
   :value (-> %1 :groups first)}
 
@@ -27,8 +27,8 @@
   :value (-> %1 :groups first)}
 
  "email"
- #"([\w\.]+@[\w\.]+)"
+ #"([\w\.\-_]+@[\w\.\-_]+)"
  {:dim :email
   :value (-> %1 :groups first)}
- 
+
 )
