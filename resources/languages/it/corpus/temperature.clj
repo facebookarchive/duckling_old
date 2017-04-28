4164fbab3bd7f9e94b5f0256022670b9801238ed
Christian Rimondi <christian.rimondi@gmail.com>
2017-04-28 14:12:50 +0200

    [IT] Added italian temperature

---
+++ resources/languages/it/corpus/temperature.clj
@@ -0,0 +1,33 @@
+(
+	; Context map
+	{}
+
+	; Temperature
+
+	"37°C"
+	"37 ° celsius"
+	"37 ° centigradi"
+	"37 gradi Celsius"
+	"37 gradi Centigradi"
+	"trentasette celsius"
+	"trentasette gradi centigradi"
+	(temperature 37 "celsius")
+
+	"1 grado centigrado"
+	(temperature 1 "celsius")
+
+	"70°F"
+	"70 ° Fahrenheit"
+	"70 gradi F"
+	"70 gradi Fahreneit"
+	"settanta Fahrenheit"
+	(temperature 70 "fahrenheit")
+
+	"45°"
+	"45 gradi"
+	(temperature 45 "degree")
+
+	"1 grado"
+	(temperature 1 "degree")
+)
+
