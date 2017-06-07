9afc867c162a9c327f41ade609f14fbdf99354bf
Dan Pomohaci <dan.pomohaci@gmail.com>
2017-06-07 12:47:08 +0300

    updates used libs

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject wit/duckling "0.4.24"
-  :description "Date & Number parser"
+(defproject wit/duckling "0.4.25-dev"
+  :description "A Clojure library that parses text into structured data"
   :license {:url "https://github.com/wit-ai/duckling"
             :comments "see LICENSE"}
   :url "https://github.com/wit-ai/duckling"
@@ -7,14 +7,14 @@
             [lein-midje "3.1.3"]]
   :repl-options {:init-ns duckling.core}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.nrepl "0.2.3"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [clj-time "0.8.0"]
-                 [prismatic/plumbing "0.5.3"]]
+                 [org.clojure/tools.nrepl "0.2.13"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [clj-time "0.13.0"]
+                 [prismatic/plumbing "0.5.4"]]
   :deploy-repositories [["clojars" {:creds :gpg}]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.6"]
-                                  [midje "1.6.3"]
-                                  [cheshire "5.3.1"]]}
+  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.9"]
+                                  [midje "1.8.3"]
+                                  [cheshire "5.7.1"]]}
              :uberjar {:aot [duckling.core]}}
   :test-selectors {:default (complement :benchmark)
                    :benchmark :benchmark}
